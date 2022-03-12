06a9e65d9f3d40e6c5803390c24c9dbfdd8cdd94
jar <jar-evans@github.com>
2022-03-12 17:27:16 +0100

    Add basic iterative methods

---
+++ iterative_lib.jl
@@ -0,0 +1,138 @@
+using LinearAlgebra
+using BenchmarkTools
+
+const MAX_ITER = 100
+
+function norm(x)
+    norm = 0
+    for i in eachindex(x)
+        @inbounds norm += x[i]^2
+    end
+    norm ^0.5
+end
+
+function jacobi(A, b, x)
+    
+    N = size(A, 1)
+    i = 0
+
+    while i < MAX_ITER
+
+        x_new = zeros(N)        
+    
+        for j in 1:N
+
+            L = A[j, 1:j-1]'*x[1:j-1]
+            U = A[j, j+1:N]'*x[j+1:N]
+
+            x_new[j] = (b[j] - L - U) / A[j, j]
+
+            (j > 1) && x_new[j] == x_new[j-1] && break
+
+        end
+
+        norm(x - x_new) < 1e-10 && break
+    
+        x = x_new
+        i += 1
+
+    end
+
+    x
+
+end
+
+function gauss_seidal(A, b, x)
+
+    N = size(A, 1)
+    i = 0
+
+    while i < MAX_ITER
+
+        x_new = zeros(N)        
+    
+        for j in 1:N
+
+            L = A[j, 1:j-1]'*x_new[1:j-1]
+            U = A[j, j+1:N]'*x[j+1:N]
+
+            x_new[j] = (b[j] - L - U) / A[j, j]
+
+            (j > 1) && x_new[j] == x_new[j-1] && break
+
+        end
+
+        norm(x - x_new) < 1e-10 && break
+    
+        x = x_new
+        i += 1
+
+    end
+
+    x
+
+end
+
+function weighted_jacobi(A, b, x, w)
+
+    N = size(A, 1)
+    i = 0
+
+    while i < MAX_ITER
+
+        x_new = zeros(N)        
+    
+        for j in 1:N
+
+            L = A[j, 1:j-1]'*x[1:j-1]
+            U = A[j, j+1:N]'*x[j+1:N]
+            W = (1 - w) * x[j]
+
+            x_new[j] = (w * (b[j] - L - U) / A[j, j]) + W
+
+            (j > 1) && x_new[j] == x_new[j-1] && break
+
+        end
+
+        norm(x - x_new) < 1e-10 && break
+    
+        x = x_new
+        i += 1
+
+    end
+
+    x
+
+end
+
+function SOR(A, b, x, w)
+
+    N = size(A, 1)
+    i = 0
+
+    while i < MAX_ITER
+
+        x_new = zeros(N)
+
+        for j in 1:N
+
+            L = A[j, 1:j-1]'*x_new[1:j-1]
+            U = A[j, j+1:N]'*x[j+1:N]
+            W = (1 - w) * x[j]
+
+            x_new[j] = (w * (b[j] - L - U) / A[j, j]) + W
+
+            (j > 1) && x_new[j] == x_new[j-1] && break
+
+        end
+
+        norm(x - x_new) < 1e-10 && break
+    
+        x = x_new
+        i += 1
+
+    end
+
+    x
+
+end
